f3709dcae0bd81d835176d2612a761ebe970a1a3
Krishna Pradeep <gss_krishnapradeep@yahoo.com>
2018-06-10 20:57:39 -0700

    Redesigning login screen

---
+++ Euphoria/Extensions/UITextField+.swift
@@ -0,0 +1,22 @@
+//
+//  UITextField+.swift
+//  Euphoria
+//
+//  Created by Krishna Pradeep on 6/10/18.
+//  Copyright © 2018 Guduru, Pradeep(AWF). All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
