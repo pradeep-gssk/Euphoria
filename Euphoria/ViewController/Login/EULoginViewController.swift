4486ed79256b88a37f04b4123ca129476358ffa5
Pradeep Guduru <pradeep.guduru@venmo.com>
2019-03-19 21:06:28 -0700

    add images and fixed bugs

--- Euphoria/ViewController/Login/EULoginViewController.swift
+++ Euphoria/ViewController/Login/EULoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EULoginViewController: UIViewController {
+class EULoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -30,6 +30,16 @@ class EULoginViewController: UIViewController {
         self.removeConcentViewIfExist()
         self.emailTextField.desginView()
         self.passwordTextField.desginView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     //TODO: show errors
@@ -58,5 +68,18 @@ class EULoginViewController: UIViewController {
     @IBAction func didTapGesture(_ sender: Any) {
         let viewController = EUConcentViewController.loadPrivacyView()
         self.navigationController?.show(viewController, sender: self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        self.view.frame.origin.y = -100
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
